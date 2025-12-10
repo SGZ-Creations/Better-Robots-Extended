7954e1b3f36d435843b2eb80fd325debf9ad7d4b
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-12-10 07:57:35 +0100

    Crash fixes

--- BetterRobotsLight/prototypes/Item/BRE-roboport.lua
+++ BetterRobotsLight/prototypes/Item/BRE-roboport.lua
@@ -1,23 +1,46 @@
+local item_sounds = require("__base__.prototypes.item_sounds")
 data:extend({
+    {
+        type = "item",
+        name = "bre-roboport-mk1",
+        icon = "__base__/graphics/icons/roboport.png",
+        subgroup = "logistic-network",
+        order = "c[signal]-a[roboport]",
+        inventory_move_sound = item_sounds.roboport_inventory_move,
+        pick_sound = item_sounds.roboport_inventory_pickup,
+        drop_sound = item_sounds.roboport_inventory_move,
+        place_result = "bre-roboport-mk1",
+        stack_size = 10,
+        weight = 100*kg,
+        random_tint_color = item_tints.iron_rust
+    },
     {
         type = "item",
         name = "bre-roboport-mk2",
         icon = "__BetterRobotsLight__/graphics/icons/bre-roboport-mk2.png",
-        icon_size = 64,
-        subgroup = "Roboport",
-        order = "ab[roboport]",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "logistic-network",
+        order = "c[signal]-ab[roboport]",
+        inventory_move_sound = item_sounds.roboport_inventory_move,
+        pick_sound = item_sounds.roboport_inventory_pickup,
+        drop_sound = item_sounds.roboport_inventory_move,
         place_result = "bre-roboport-mk2",
         stack_size = 10,
+        weight = 100*kg,
     },
     {
         type = "item",
         name = "bre-roboport-mk3",
         icon = "__BetterRobotsLight__/graphics/icons/bre-roboport-mk3.png",
-        icon_size = 64,
-        subgroup = "Roboport",
-        order = "ac[roboport]",
+        icon_size = 64, icon_mipmaps = 4,
+        subgroup = "logistic-network",
+        order = "c[signal]-ac[roboport]",
+        inventory_move_sound = item_sounds.roboport_inventory_move,
+        pick_sound = item_sounds.roboport_inventory_pickup,
+        drop_sound = item_sounds.roboport_inventory_move,
         place_result = "bre-roboport-mk3",
         stack_size = 10,
+        weight = 100*kg,
     }
 })
 data.raw["item"]["roboport"].order = "aa[roboport]"
--- BetterRobotsLight/prototypes/Recipe/Roboport.lua
+++ BetterRobotsLight/prototypes/Recipe/Roboport.lua
@@ -1,16 +1,27 @@
 data:extend({
     {
         type = "recipe",
+        name = "bre-roboport-mk1",
+        enabled = false,
+        allow_quality = true,
+        ingredients = {
+            {type = "item", name = "steel-plate", amount = 20 },
+            {type = "item", name = "iron-plate", amount = 20 },
+            {type = "item", name = "electronic-circuit", amount = 20 }
+        },
+        results = {{ type = "item", name = "bre-roboport-mk1", amount = 1 }}
+    },
+    {
+        type = "recipe",
         name = "bre-roboport-mk2",
         enabled = false,
         allow_quality = true,
         energy_required = 10,
-        ingredients =
-        {
-            {type = "item", name = "steel-plate", amount = 10},
+        ingredients = {
             {type = "item", name = "roboport", amount = 2},
             {type = "item", name = "battery", amount = 45},
-            {type = "item", name = "processing-unit", amount = 5}
+            {type = "item", name = "advanced-circuit", amount = 5},
+            {type = "item", name = "low-density-structure", amount = 10},
         },
         results = {
             {type = "item", name = "bre-roboport-mk2", amount = 1}
@@ -22,12 +33,10 @@ data:extend({
         enabled = false,
         allow_quality = true,
         energy_required = 20,
-        ingredients =
-        {
-            {type = "item", name = "steel-plate", amount = 25},
+        ingredients = {
             {type = "item", name = "bre-roboport-mk2", amount = 2},
-            {type = "item", name = "efficiency-module-2", amount = 5},
-            {type = "item", name = "processing-unit", amount = 15}
+            {type = "item", name = "processing-unit", amount = 15},
+            {type = "item", name = "low-density-structure", amount = 10},
         },
         results = {
             {type = "item", name = "bre-roboport-mk3", amount = 1}
