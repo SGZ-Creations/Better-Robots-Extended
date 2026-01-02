1b69687d86ba5f4711eddc04fff7dbffd1012d6c
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-02 12:54:02 +0100

    fixed bobs crrash new tumbnail

--- BetterRobotsLight/Compatibilities/Bobs/BobRecipesFixes.lua
+++ BetterRobotsLight/Compatibilities/Bobs/BobRecipesFixes.lua
@@ -16,7 +16,7 @@ end
 
 Recipe["roboport"].ingredients = nil
 
-if mods["boblogistics"]then
+if mods["boblogistics"]and not mods["bobplates"]then
 	table.insert(Recipe["BRL-construction-robotics-mk2"].ingredients, {type = "item", name = "bob-flying-robot-frame-2", amount = 1})
 	table.insert(Recipe["BRL-construction-robotics-mk3"].ingredients, {type = "item", name = "bob-flying-robot-frame-3", amount = 1})
 	table.insert(Recipe["BRL-construction-robotics-mk4"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
