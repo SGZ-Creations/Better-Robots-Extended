948215575f571f18a576c5370fad50b9b61262f0
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-07 07:48:53 +0100

    Bobs Settings Crashes

--- BetterRobotsLight/Compatibilities/Bobs/BobRecipesFixes.lua
+++ BetterRobotsLight/Compatibilities/Bobs/BobRecipesFixes.lua
@@ -1,6 +1,7 @@
 ---@class data.RecipePrototype
 local Recipe = data.raw.recipe
-
+---@class LuaSettings
+local SS = settings.startup
 
 function RemoveIngredients(recipe_name, ingredient_name)
     for i, ingredient in pairs(Recipe[recipe_name].ingredients) do
@@ -17,22 +18,24 @@ end
 Recipe["roboport"].ingredients = nil
 
 if mods["boblogistics"]and not mods["bobplates"]then
-	table.insert(Recipe["BRL-construction-robotics-mk2"].ingredients, {type = "item", name = "bob-flying-robot-frame-2", amount = 1})
-	table.insert(Recipe["BRL-construction-robotics-mk3"].ingredients, {type = "item", name = "bob-flying-robot-frame-3", amount = 1})
-	table.insert(Recipe["BRL-construction-robotics-mk4"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
-	table.insert(Recipe["BRL-construction-robotics-mk5"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
-	table.insert(Recipe["BRL-logistic-robotics-mk2"].ingredients, {type = "item", name = "bob-flying-robot-frame-2", amount = 1})
-	table.insert(Recipe["BRL-logistic-robotics-mk3"].ingredients, {type = "item", name = "bob-flying-robot-frame-3", amount = 1})
-	table.insert(Recipe["BRL-logistic-robotics-mk4"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
-	table.insert(Recipe["BRL-logistic-robotics-mk5"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
+	if SS["bobmods-logistics-flyingrobotframes"].value == true then
+		table.insert(Recipe["BRL-construction-robotics-mk2"].ingredients, {type = "item", name = "bob-flying-robot-frame-2", amount = 1})
+		table.insert(Recipe["BRL-construction-robotics-mk3"].ingredients, {type = "item", name = "bob-flying-robot-frame-3", amount = 1})
+		table.insert(Recipe["BRL-construction-robotics-mk4"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
+		table.insert(Recipe["BRL-construction-robotics-mk5"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
+		table.insert(Recipe["BRL-logistic-robotics-mk2"].ingredients, {type = "item", name = "bob-flying-robot-frame-2", amount = 1})
+		table.insert(Recipe["BRL-logistic-robotics-mk3"].ingredients, {type = "item", name = "bob-flying-robot-frame-3", amount = 1})
+		table.insert(Recipe["BRL-logistic-robotics-mk4"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
+		table.insert(Recipe["BRL-logistic-robotics-mk5"].ingredients, {type = "item", name = "bob-flying-robot-frame-4", amount = 1})
 
-	RemoveIngredients("BRL-construction-robotics-mk2", "flying-robot-frame")
-	RemoveIngredients("BRL-construction-robotics-mk3", "flying-robot-frame")
-	RemoveIngredients("BRL-construction-robotics-mk4", "flying-robot-frame")
-	RemoveIngredients("BRL-construction-robotics-mk5", "flying-robot-frame")
+		RemoveIngredients("BRL-construction-robotics-mk2", "flying-robot-frame")
+		RemoveIngredients("BRL-construction-robotics-mk3", "flying-robot-frame")
+		RemoveIngredients("BRL-construction-robotics-mk4", "flying-robot-frame")
+		RemoveIngredients("BRL-construction-robotics-mk5", "flying-robot-frame")
 
-	RemoveIngredients("BRL-logistic-robotics-mk2", "flying-robot-frame")
-	RemoveIngredients("BRL-logistic-robotics-mk3", "flying-robot-frame")
-	RemoveIngredients("BRL-logistic-robotics-mk4", "flying-robot-frame")
-	RemoveIngredients("BRL-logistic-robotics-mk5", "flying-robot-frame")
+		RemoveIngredients("BRL-logistic-robotics-mk2", "flying-robot-frame")
+		RemoveIngredients("BRL-logistic-robotics-mk3", "flying-robot-frame")
+		RemoveIngredients("BRL-logistic-robotics-mk4", "flying-robot-frame")
+		RemoveIngredients("BRL-logistic-robotics-mk5", "flying-robot-frame")
+	end
 end
--- BetterRobotsLight/Compatibilities/Bobs/BobTech.lua
+++ BetterRobotsLight/Compatibilities/Bobs/BobTech.lua
@@ -25,15 +25,16 @@ if mods["boblogistics"] then
 
 	Tech["worker-robots-speed-2"].prerequisites = {"worker-robots-speed-1"}
 	Tech["worker-robots-storage-1"].prerequisites = {"robotics"}
+	if SS["bobmods-logistics-flyingrobotframes"].value == true then
+		Tech["bob-robotics-3"].unit.ingredients = {
+			{"automation-science-pack", 1},
+			{"logistic-science-pack", 1},
+			{"chemical-science-pack", 1},
+			{"production-science-pack", 1},
+		}
 
-	Tech["bob-robotics-3"].unit.ingredients = {
-		{"automation-science-pack", 1},
-		{"logistic-science-pack", 1},
-		{"chemical-science-pack", 1},
-		{"production-science-pack", 1},
-	}
-
-	table.insert(Tech["BRL-RoboticsAndPorts-2"].prerequisites, "bob-robotics-2")
-	table.insert(Tech["BRL-RoboticsAndPorts-3"].prerequisites, "bob-robotics-3")
-	table.insert(Tech["BRL-RoboticsAndPorts-4"].prerequisites, "bob-robotics-4")
+		table.insert(Tech["BRL-RoboticsAndPorts-2"].prerequisites, "bob-robotics-2")
+		table.insert(Tech["BRL-RoboticsAndPorts-3"].prerequisites, "bob-robotics-3")
+		table.insert(Tech["BRL-RoboticsAndPorts-4"].prerequisites, "bob-robotics-4")
+	end
 end
