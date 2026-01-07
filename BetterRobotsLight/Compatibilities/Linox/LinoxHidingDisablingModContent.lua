0d3ecbc3ae9a71603345a50a1eea3d664b4ad27d
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-07 07:50:47 +0100

    linox Hiding incomplete

--- BetterRobotsLight/Compatibilities/Linox/LinoxHidingDisablingModContent.lua
+++ BetterRobotsLight/Compatibilities/Linox/LinoxHidingDisablingModContent.lua
@@ -2,8 +2,11 @@
 local Item = data.raw.item
 ---@class data.RecipePrototype
 local Recipe = data.raw.recipe
-
+local Data = data.raw
 
 if mods["dea-dia-system"]then
     data.raw["roboport"]["gas-collector-roboport"].next_upgrade = nil
+
+    --Data["construction-robot"]["construction-robot"].hidden_in_factoriopedia = true
+    --Data["logistic-robot"]["logistic-robot"].hidden_in_factoriopedia = true
 end
