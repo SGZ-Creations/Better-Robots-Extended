1d245877df6446b3316ef62907b1bda206a672de
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-07 07:58:34 +0100

    Inverted code locations.

--- BetterRobotsLight/Compatibilities/DiaDie/DiaDieHidingDisablingModContent.lua
+++ BetterRobotsLight/Compatibilities/DiaDie/DiaDieHidingDisablingModContent.lua
@@ -2,30 +2,9 @@
 local Item = data.raw.item
 ---@class data.RecipePrototype
 local Recipe = data.raw.recipe
----@class Data.TechnologyPrototype
-local Tech = data.raw["technology"]
+local Data = data.raw
 
-local function Hide_Item(name)
-    local item = Item[name]
-    if item then
-        item.hidden = true
-    end
-    if not (item) then
-        log("Could not find "..name)
-    end
-end
-
-if mods["linox"]then
-    data.raw["logistic-robot"]["linox_samarium-logistic-robot"].next_upgrade = nil
-    data.raw["construction-robot"]["linox_samarium-construction-robot"].next_upgrade = nil
-    table.insert(Recipe["BRL-logistic-robotics-mk5"].ingredients, {type = "item", name = "samarium", amount = 10})
-    table.insert(Recipe["BRL-construction-robotics-mk5"].ingredients, {type = "item", name = "samarium", amount = 10})
-    table.insert(Tech["BRL-RoboticsAndPorts-5"].prerequisites, "linox-technology_sintering-machine")
-
-    Recipe["linox_samarium-logistic-robot"].hidden = true
-    Recipe["linox_samarium-logistic-robot"].enabled = false
-    Recipe["linox_samarium-construction-robot"].hidden = true
-    Recipe["linox_samarium-construction-robot"].enabled = false
-    Hide_Item("linox_samarium-logistic-robot")
-    Hide_Item("linox_samarium-construction-robot")
+if mods["dea-dia-system"]then
+    Data["roboport"]["gas-collector-roboport"].next_upgrade = nil
+    Data["roboport"]["gas-collector-roboport"].hidden_in_factoriopedia = true
 end
--- BetterRobotsLight/Compatibilities/Linox/LinoxHidingDisablingModContent.lua
+++ BetterRobotsLight/Compatibilities/Linox/LinoxHidingDisablingModContent.lua
@@ -2,11 +2,34 @@
 local Item = data.raw.item
 ---@class data.RecipePrototype
 local Recipe = data.raw.recipe
+---@class Data.TechnologyPrototype
+local Tech = data.raw["technology"]
 local Data = data.raw
 
-if mods["dea-dia-system"]then
-    data.raw["roboport"]["gas-collector-roboport"].next_upgrade = nil
+local function Hide_Item(name)
+    local item = Item[name]
+    if item then
+        item.hidden = true
+    end
+    if not (item) then
+        log("Could not find "..name)
+    end
+end
 
-    --Data["construction-robot"]["construction-robot"].hidden_in_factoriopedia = true
-    --Data["logistic-robot"]["logistic-robot"].hidden_in_factoriopedia = true
+if mods["linox"]then
+    data.raw["logistic-robot"]["linox_samarium-logistic-robot"].next_upgrade = nil
+    data.raw["construction-robot"]["linox_samarium-construction-robot"].next_upgrade = nil
+    table.insert(Recipe["BRL-logistic-robotics-mk5"].ingredients, {type = "item", name = "samarium", amount = 10})
+    table.insert(Recipe["BRL-construction-robotics-mk5"].ingredients, {type = "item", name = "samarium", amount = 10})
+    table.insert(Tech["BRL-RoboticsAndPorts-5"].prerequisites, "linox-technology_sintering-machine")
+
+    Recipe["linox_samarium-logistic-robot"].hidden = true
+    Recipe["linox_samarium-logistic-robot"].enabled = false
+    Recipe["linox_samarium-construction-robot"].hidden = true
+    Recipe["linox_samarium-construction-robot"].enabled = false
+    Hide_Item("linox_samarium-logistic-robot")
+    Hide_Item("linox_samarium-construction-robot")
+
+    Data["construction-robot"]["linox_samarium-construction-robot"].hidden_in_factoriopedia = true
+    Data["logistic-robot"]["linox_samarium-logistic-robot"].hidden_in_factoriopedia = true
 end
